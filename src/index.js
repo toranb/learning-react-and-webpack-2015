fecddc7fd3fcd7596c3abaca0d3a72d980036501
toran billups <toranb@gmail.com>
2015-10-22 17:17:10 -0500

    reboot

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-import Users from './users';
-import User from './user';
-import Home from './home';
-import { Router, Route, IndexRoute } from 'react-router';
 
-class NoMatch extends React.Component {
+class HelloWorld extends React.Component {
     render() {
-        return <div>404 son!</div>;
+        return <div>hello</div>;
     }
 };
 
 ReactDOM.render((
-  <Router>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="users" component={Users}>
-        <Route path=":userId" component={User}/>
-      </Route>
-      <Route path="*" component={NoMatch}/>
-    </Route>
-  </Router>
-), document.querySelector('#container'));
+  <HelloWorld />
+), document.getElementById('container'));
--- src/user.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class User extends React.Component {
-    render() {
-        return <div>UserId: {this.props.params.userId}</div>;
-    }
-};
-
-export default User;
