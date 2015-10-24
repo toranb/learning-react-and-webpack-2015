f00787440552826e5f37982e5d9f5a1e75e83238
toran billups <toranb@gmail.com>
2015-10-24 10:21:16 -0500

    moved the hello component to a standalone file

---
+++ src/hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render() {
+        return <div>hello</div>;
+    }
+};
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class HelloWorld extends React.Component {
-    render() {
-        return <div>hello</div>;
-    }
-};
+import HelloWorld from './hello';
 
 ReactDOM.render((
   <HelloWorld />
