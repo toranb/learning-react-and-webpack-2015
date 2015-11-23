033dbf6de60acc04cce099eca9f8a19c406b049c
toran billups <toranb@gmail.com>
2015-11-22 19:44:53 -0600

    added beforeEach to fix url after a e2e test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import HelloWorld from '../src/hello.jsx';
-import { render } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import click from './click';
 import execSteps from './execSteps';
 import Routes from '../src/routes.jsx';
@@ -11,7 +11,7 @@ var component, node;
 
 describe('Given an instance of the Component', () => {
     describe('when we render the component', () => {
-        before(() => {
+        beforeEach(function() {
             component = TestUtils.renderIntoDocument(<HelloWorld />);
         });
         it('should render a div with hello', () => {
@@ -21,10 +21,45 @@ describe('Given an instance of the Component', () => {
     });
 
     describe('when we render the application', () => {
-        before(() => {
+        beforeEach(function(done) {
             node = document.createElement('div');
+            if (window.location.hash !== '') {
+                window.location.hash = '';
+                setTimeout(done, 10);
+            } else {
+                done();
+            }
+        });
+        afterEach(function() {
+            unmountComponentAtNode(node);
         });
         it('should render person details when clicking through from users', (done) => {
+            var steps = [
+                function () {
+                    var users = node.querySelector('a');
+                    expect(users.getAttribute('href')).eql('#/users');
+                    click(users);
+                },
+                function () {
+                    var user = node.querySelector('li:first-child a');
+                    expect(user.getAttribute('href')).eql('#/users/1');
+                    click(user);
+                },
+                function () {
+                    // var activity = node.querySelector('.activity-log');
+                    expect(1).eql(1);
+                }
+            ];
+            var execNextStep = execSteps(steps, done);
+            var Route = (
+                <Router onUpdate={execNextStep}>
+                    {Routes}
+                </Router>
+            );
+            render((Route), node, execNextStep);
+        });
+        // copy pasted
+        it('also it will render person details when clicking through from users', (done) => {
             var steps = [
                 function () {
                     var users = node.querySelector('a');
